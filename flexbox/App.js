f5d50160dcd305f3ac3c419e4acc20aa0f2913e2
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2020-01-02 15:14:29 -0300

    dinamicamente altera layout

--- flexbox/App.js
+++ flexbox/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 
 function vermelha() {
   return (
@@ -24,22 +24,80 @@ const azul = () => (
 );
 
 export default function App() {
+  const [flexDirection, setFlexDirection] = useState("column");
+  const [reverse, setReverse] = useState("");
+
+  const fullName = (d, r) => d + r;
+  const reverseName = () => (reverse !== "" ? "normal" : "reverse");
+
+  const changeDirection = direction => {
+    setFlexDirection(fullName(direction, reverse));
+    console.log(flexDirection);
+  };
+
+  const changeReverse = () => {
+    const row = flexDirection.startsWith("row");
+    const sufixo = reverse === "" ? "-reverse" : "";
+    const direcao = row ? "row" : "column";
+    setReverse(sufixo);
+    setFlexDirection(fullName(direcao, sufixo));
+  };
+
   return (
     <View style={styles.container}>
-      {vermelha()}
-      {verde()}
-      {azul()}
+      <View style={[styles.work, { flexDirection: flexDirection }]}>
+        {vermelha()}
+        {verde()}
+        {azul()}
+      </View>
+      <View style={styles.opcoes}>
+        <Text style={styles.info}>flexDirection : {flexDirection}</Text>
+        <View style={styles.botoes}>
+          <Button title="row" onPress={() => changeDirection("row")} />
+          <Button title="column" onPress={() => changeDirection("column")} />
+          <TouchableOpacity
+            style={styles.botaoReverse}
+            onPress={() => changeReverse()}
+          >
+            <Text>{reverseName()}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  info: {
+    fontSize: 20,
+    textAlign: "center",
+    padding: 20
+  },
+
+  botoes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+
+  botaoReverse: {
+    alignItems: "center",
+    backgroundColor: "lightblue",
+    padding: 15
+  },
+
+  opcoes: {},
+
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-around"
+    backgroundColor: "#fff"
+  },
+
+  work: {
+    height: "60%",
+    borderWidth: 1,
+    borderColor: "grey",
+    margin: 35
   },
 
   red: {
