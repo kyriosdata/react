6f70e049de4eabc031498796f9bb6bd1029b8be1
kyriosdata <fabio@inf.ufg.br>
2020-01-24 17:03:41 -0300

    preparacao para redux

--- redux-preferencias/screens/TelaA.js
+++ redux-preferencias/screens/TelaA.js
@@ -4,18 +4,43 @@ import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 export default TelaA = props => {
   const [cor, setCor] = useState('');
 
+  function Botoes() {
+    return (
+      <View style={styles.botoes}>
+        <Button title="Vá para Cor"
+          onPress={() => props.navigation.navigate({ routeName: "B" })}
+        />
+
+        <Button
+          title="Vá para Número"
+          onPress={() => props.navigation.navigate({ routeName: "B" })}
+        />
+      </View>
+    );
+  }
+
+  const Entradas = (props) => {
+    return (
+      <View style={styles.entradas}>
+        <Text>Minha cor preferida é</Text>
+        <TextInput style={styles.entrada}
+          onChangeText={texto => setCor(texto)}
+          value={cor}
+        />
+
+        <Text>Meu número preferido:</Text>
+        <TextInput style={styles.entrada}
+          onChangeText={texto => setCor(texto)}
+          value={cor}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.principal}>
-      <Text>Minha cor preferida é:</Text>
-      <TextInput style={styles.entrada}
-        onChangeText={texto => setCor(texto)}
-        value={cor}
-      />
-
-      <Button
-        title="Vá para B"
-        onPress={() => props.navigation.navigate({ routeName: "B" })}
-      />
+      <Entradas />
+      <Botoes />
     </View>
   );
 };
@@ -27,10 +52,21 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
 
+  entradas: {
+    alignItems: 'center',
+  },
+
   entrada: {
     borderBottomWidth: 1,
     borderBottomColor: 'black',
     marginVertical: 20,
     width: 100,
-  }
+  },
+
+  botoes: {
+    flexDirection: 'row',
+    width: '70%',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
 });
