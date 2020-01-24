cd967bc77f75b4e451c7eda05ee8558c0b0e4b00
kyriosdata <fabio@inf.ufg.br>
2020-01-24 10:57:58 -0300

    atualizacao

--- redux/App.js
+++ redux/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createStore } from "redux";
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case "ADICIONA":
+      return state + 1;
+    case "SUBTRAI":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+let store = createStore(counter);
 
 export default function App() {
+  console.log('rendering...');
+  const [contador, setContador] = useState(store.getState());
+
+  store.subscribe(() => setContador(store.getState()));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Selecione uma operação</Text>
+      <View style={styles.linhaBotoes}>
+        <Button
+          title="ADICIONA"
+          onPress={() => store.dispatch({ type: "ADICIONA" })}
+        />
+        <Button
+          title="SUBTRAI"
+          onPress={() => store.dispatch({ type: "SUBTRAI" })}
+        />
+      </View>
+
+      <View>
+        <Text style={styles.resultado}>{contador}</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +45,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+
+  linhaBotoes: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: 200
+  },
+
+  resultado: {
+    fontSize: 66,
+    fontWeight: "bold",
+    marginTop: 45
+  }
 });
