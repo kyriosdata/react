5cd52d37f70c758bfe13662273493fcea8d2c7b3
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2020-03-17 10:40:06 -0300

    documentação

--- redux/App.js
+++ redux/App.js
@@ -1,38 +1,41 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import { createStore } from "redux";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStore } from 'redux';
 
+/*
+  REDUCER. 'state' ou estado inicial é um valor numérico, iniciado com 0. 
+  A ação (action) é um objeto que pressupõe o atributo 'type' (simples convenção). 
+  A cada execução do reducer um novo estado é produzido e retornado. 
+*/
 function counter(state = 0, action) {
   switch (action.type) {
-    case "ADICIONA":
+    case 'ADICIONA':
       return state + 1;
-    case "SUBTRAI":
+    case 'SUBTRAI':
       return state - 1;
     default:
       return state;
   }
 }
 
+// Todo o estado da aplicação
 let store = createStore(counter);
 
+const action = tipo => store.dispatch({ type: tipo });
+
 export default function App() {
-  console.log("rendering...");
   const [contador, setContador] = useState(store.getState());
 
+  // Registra função a ser chamada toda vez que houver mudança de estado
+  // Neste caso, a função altera a variável 'contador' que força redraw.
   store.subscribe(() => setContador(store.getState()));
 
   return (
     <View style={styles.container}>
       <Text>Selecione uma operação</Text>
       <View style={styles.linhaBotoes}>
-        <Button
-          title="ADICIONA"
-          onPress={() => store.dispatch({ type: "ADICIONA" })}
-        />
-        <Button
-          title="SUBTRAI"
-          onPress={() => store.dispatch({ type: "SUBTRAI" })}
-        />
+        <Button title="ADICIONA" onPress={action.bind(this, 'ADICIONA')} />
+        <Button title="SUBTRAI" onPress={action.bind(this, 'SUBTRAI')} />
       </View>
 
       <View>
@@ -45,20 +48,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   linhaBotoes: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: 200
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 200,
   },
 
   resultado: {
     fontSize: 66,
-    fontWeight: "bold",
-    marginTop: 45
-  }
+    fontWeight: 'bold',
+    marginTop: 45,
+  },
 });
