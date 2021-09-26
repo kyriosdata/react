1c61f256cdbda24b0f889c9af8d0b3962fdb4cc4
kyriosdata <qp02nv*$>
2021-09-26 19:51:32 -0300

    comments

---
+++ micro-services/comments/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  const comments = commentsByPostId[req.params.id] || [];
+  res.send(comments);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const id = uuidv4();
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({
+    id,
+    ...req.body,
+  });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Listening on port 4001...");
+});
